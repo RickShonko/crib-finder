5a420caa6b1c03b6225a3e139c0cd1ec75f1bfa7
Rick <moitalel.dev@gmail.com>
2026-01-22 10:47:00 +0300

    edited fav, and cons

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -42,7 +42,7 @@ export function Footer() {
               </div>
               <div className="flex items-center gap-2">
                 <Phone className="h-4 w-4" />
-                <span>+254 700 000 000</span>
+                <span>+254 745 922 797</span>
               </div>
             </div>
           </div>
